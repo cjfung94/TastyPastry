3c557a3ebfb65dd2e309efc09d0de8bfaa67b166
Jorge Esquivel <je82382n@pace.edu>
2021-03-18 22:57:15 -0400

    Partially working Nav Bar

--- Tasty Pastry/app/src/main/java/com/example/tastypastry/Favorites.java
+++ Tasty Pastry/app/src/main/java/com/example/tastypastry/Favorites.java
@@ -1,60 +1,37 @@
 package com.example.tastypastry;
 
-import android.app.Activity;
-import android.content.Context;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-
-import androidx.annotation.NonNull;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.mindorks.placeholderview.SwipeDecor;
-import com.mindorks.placeholderview.SwipePlaceHolderView;
 
-public class DashBoardActivity extends Activity {
-    private Button logout;
-    private SwipePlaceHolderView testSwipe;
-    private Context testContext;
-
+public class Favorites extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dashboard);
-        //Swiping stuff
-        testSwipe = (SwipePlaceHolderView)findViewById(R.id.swipeView);
-        testContext = getApplicationContext();
-
-        testSwipe.getBuilder()
-                .setDisplayViewCount(3)
-                .setSwipeDecor(new SwipeDecor()
-                        .setPaddingTop(20)
-                        .setRelativeScale(0.01f));
-        for (Profile profile : Utils.loadPictures(this.getApplicationContext())){
-            testSwipe.addView(new SwipeFunction(testContext, profile, testSwipe));
-        }
-
+        setContentView(R.layout.activity_favorites);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setSelectedItemId(R.id.Favorites);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()) {
                     case R.id.Home:
+                        startActivity(new Intent(getApplicationContext(), DashBoardActivity.class));
+                        overridePendingTransition(0,0);
                         return true;
                     case R.id.Filter:
                         startActivity(new Intent(getApplicationContext(), Filter.class));
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.Favorites:
-                        startActivity(new Intent(getApplicationContext(), Favorites.class));
-                        overridePendingTransition(0,0);
                         return true;
                     case R.id.Settings:
                         startActivity(new Intent(getApplicationContext(), Settings.class));
@@ -64,7 +41,5 @@ public class DashBoardActivity extends Activity {
                 return false;
             }
         });
-
     }
 }
-
