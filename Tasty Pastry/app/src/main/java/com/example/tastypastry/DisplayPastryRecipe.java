a9562fb0d1ede4b639ae3cdefc9207aff65175bb
Maryia <mk70123n@pace.edu>
2021-03-22 23:06:13 -0400

    Fixed layout (in process)

--- Tasty Pastry/app/src/main/java/com/example/tastypastry/DisplayPastryRecipe.java
+++ Tasty Pastry/app/src/main/java/com/example/tastypastry/DisplayPastryRecipe.java
@@ -1,14 +1,54 @@
 package com.example.tastypastry;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class DisplayPastryRecipe extends AppCompatActivity {
+
+    private ImageView PastryImage;
+    private Profile testProfile;
+    private ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.recipes);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.Home:
+                        startActivity(new Intent(getApplicationContext(), DashBoardActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.Filter:
+                        startActivity(new Intent(getApplicationContext(), Filter.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.Favorites:
+                        startActivity(new Intent(getApplicationContext(), Favorites.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.Settings:
+                        startActivity(new Intent(getApplicationContext(), Settings.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
