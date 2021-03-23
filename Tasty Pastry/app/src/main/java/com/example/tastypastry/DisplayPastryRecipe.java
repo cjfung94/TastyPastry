16c50ed8341c643a10be1da8dfdf9ba65140a2b1
Maryia <mk70123n@pace.edu>
2021-03-22 21:31:18 -0400

    Added ScrollView and fixed DisplayPastryRecipe layout

--- Tasty Pastry/app/src/main/java/com/example/tastypastry/DisplayPastryRecipe.java
+++ Tasty Pastry/app/src/main/java/com/example/tastypastry/DisplayPastryRecipe.java
@@ -1,36 +1,14 @@
 package com.example.tastypastry;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DisplayPastryRecipe extends AppCompatActivity {
 
-    private ImageView PastryImage;
-    private Profile testProfile;
-    private ProgressDialog progressDialog;
-
-//    @View(R.id.IngredientsList)
-//    private TextView IngredientsList;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.recipes);
-
-
-       setContentView(R.layout.recipes);
-
-    }
-
-    public void openRecipe(View v) {
-//        setContentView(R.layout.recipes);
-//        IngredientsList.setText(testProfile.getIngredients());
-
     }
 }
