2c7bc076b0e0c2b68425f72415622ab51996c8e6
Jorge Esquivel <je82382n@pace.edu>
2021-03-25 23:54:40 -0400

    Settings.java partially complete

--- Tasty Pastry/app/src/main/java/com/example/tastypastry/Settings.java
+++ Tasty Pastry/app/src/main/java/com/example/tastypastry/Settings.java
@@ -6,15 +6,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Settings extends AppCompatActivity {
+    private Button aboutButton, helpButton, lFBButton, signOutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        aboutButton = findViewById(R.id.About_button);
+        helpButton = findViewById(R.id.Help_button);
+        lFBButton = findViewById(R.id.Leave_feedback_button);
+        signOutButton = findViewById(R.id.Sign_out_button);
+
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Something
+            }
+        });
+        helpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Something
+            }
+        });
+        lFBButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Something
+            }
+        });
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Something
+            }
+        });
+
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
 
         bottomNavigationView.setSelectedItemId(R.id.Settings);
