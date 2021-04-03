e01184a83b2580f00c41b5a1bd6e0921406338b3
Jorge Esquivel <je82382n@pace.edu>
2021-04-03 14:53:46 -0400

    Help activity design

--- Tasty Pastry/app/src/main/java/com/example/tastypastry/HelpActivity.java
+++ Tasty Pastry/app/src/main/java/com/example/tastypastry/HelpActivity.java
@@ -9,5 +9,6 @@ public class HelpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
     }
 }
