6a0a218f5cf72dfb9a3f6070b61679c6f6e4b27d
Jorge Esquivel <je82382n@pace.edu>
2021-04-04 11:12:59 -0400

    Updated help.xml again

--- Tasty Pastry/app/src/main/java/com/example/tastypastry/HelpActivity.java
+++ Tasty Pastry/app/src/main/java/com/example/tastypastry/HelpActivity.java
@@ -3,8 +3,12 @@ package com.example.tastypastry;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
+    private EditText nameSec, emailSec, subSec, commentSec;
+    private TextView helpTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
